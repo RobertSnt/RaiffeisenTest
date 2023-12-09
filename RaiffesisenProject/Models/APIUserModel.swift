771e4b75b7cf211536418892377f922de465a99f
Robert Saniuta <robert.snt@yahoo.com>
2023-12-09 14:16:09 +0200

    Api Data Models

---
+++ RaiffesisenProject/Models/APIUserModel.swift
@@ -0,0 +1,8 @@
+//
+//  APIUserModel.swift
+//  RaiffesisenProject
+//
+//  Created by Robert Saniuta on 09.12.2023.
+//
+
+import Foundation
---
+++ RaiffesisenProject/Models/LocationModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationModel.swift
+//  RaiffesisenProject
+//
+//  Created by Robert Saniuta on 09.12.2023.
+//
+
+import Foundation
