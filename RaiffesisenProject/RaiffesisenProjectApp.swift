aba70d741273ccdf766ced5f78053a9b2fbf807e
Robert Saniuta <robert.snt@yahoo.com>
2023-12-09 14:01:50 +0200

    Initial Commit

---
+++ RaiffesisenProject/RaiffesisenProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaiffesisenProjectApp.swift
+//  RaiffesisenProject
+//
+//  Created by Robert Saniuta on 09.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct RaiffesisenProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
